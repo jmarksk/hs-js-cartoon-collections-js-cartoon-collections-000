9944cccbeb34a795b376e612480e5dc7274bf54e
Jon Marks <jmarksk@gmail.com>
2019-03-11 14:08:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 function dwarfRollCall(dwarves) {
-}
 
+  var newStr = "";
+  dwarves.forEach(function(name, idx) {
+  	newStr += (idx+1 + ". " + name + " ");
+  });
+  return newStr;
+}
 function summonCaptainPlanet(planeteerCalls){
+  return planeteerCalls.map(function(ele) {
+  return ele.toUpperCase() + "!";
+});
 }
 
 function longPlaneteerCalls(words) {
+  for(var i=0; i<words.length; i++){
+    if (words[i].length>4){
+      return true
+    }
+  }
+  return false
 }
 
-function findTheCheese (foods) {
-}
+
+function findTheCheese (Foods) {
+  for(var i=0;i<Foods.length;i++){
+   if (Foods[i]=="cheddar" ||Foods[i]=="gouda" ||Foods[i]=="camembert"){
+     return Foods[i]}
+   }
+ return "no cheese!"
+ }
